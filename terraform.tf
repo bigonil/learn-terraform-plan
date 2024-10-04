1f5ffbfac58dccbe5523212373c867fac2c42ffe
bigonil <luca.bigoni@gmail.com>
2024-10-04 22:06:39 +0200

    Update version

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.7.0"
+      version = ">= 5.70.0"
     }
     random = {
       source  = "hashicorp/random"
